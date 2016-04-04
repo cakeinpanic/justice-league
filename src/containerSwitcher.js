1479ad90dede2ce024d7e580aaa4f425a058d288
cakeinpanic <cakeinpanic@gmail.com>
2016-04-05 00:42:39 +0300

    Usenanother container switcher

--- src/containerSwitcher.js
+++ src/containerSwitcher.js
@@ -1,15 +1,16 @@
-var currencySwitcher = document.getElementById('currencySwitcher');
+var containerSwitcher = document.querySelector('.containerSwitcher');
 var container = document.querySelector('.chartContainer');
 var drawChart = require('./chart.js');
 
 module.exports = {
 	init: function() {
-		currencySwitcher.addEventListener('change', switchCurrency);
+		containerSwitcher.addEventListener('change', switchView);
 	}
 };
 
-function switchCurrency() {
-	if (currencySwitcher.checked) {
+function switchView() {
+	var options = document.getElementsByName('sphere');
+	if (options[0].checked) {
 		container.classList.remove('chartContainer-showIt');
 		drawChart.drawCommon();
 	} else {
