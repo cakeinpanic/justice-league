9ba953297168ea823c5c7d7000e8243309a6708b
epavlenko <epavlenko@devexperts.com>
2016-04-13 19:18:57 +0300

    Improve private switchers

--- src/containerSwitcher.js
+++ src/containerSwitcher.js
@@ -1,41 +1,66 @@
 var globalSwitcher = document.querySelector('.containerSwitcher-global');
 var allChartsContainer = document.querySelector('.allChartsContainer');
+var localSwitchers = [];
 var drawChart = require('./chart.js');
 
-var counter = 0;
 module.exports = {
 	init: function() {
-		globalSwitcher.addEventListener('change', switchView);
+		globalSwitcher.addEventListener('change', switchGlobalView);
 	},
 	createSwitcher: createSwitcher
 };
 
+function closest(element, selector) {
+	while (element && element.nodeType === 1) {
+		if (element.matches(selector)) {
+			return element;
+		}
+		element = element.parentNode;
+	}
+	return null;
+}
+
 function switchView(e) {
-	var options = e.target.closest('.containerSwitcher').querySelectorAll('input');
-	var chartConainer = e.target.closest('.chartContainer');
+	switchLocalView(e.target);
+}
 
-	var id = chartConainer ? chartConainer.id : null;
-	var container = chartConainer ? chartConainer : allChartsContainer;
+function switchLocalView(switcher, override, showIt) {
+	var chartContainer = closest(switcher, '.chartContainer');
+	var options = closest(switcher, '.containerSwitcher').querySelectorAll('input');
 
-	if (options[0].checked) {
-		container.classList.remove('chartContainer-showIt');
+	var id = chartContainer ? chartContainer.id : null;
+	showIt = override ? showIt : options[0].checked;
+
+	if (showIt) {
 		drawChart.drawCommon(id);
 	} else {
 		drawChart.drawIt(id);
-		container.classList.add('chartContainer-showIt');
 	}
+
+	if (override) {
+		options[0].checked = showIt;
+		options[1].checked = !showIt;
+	}
+}
+
+function switchGlobalView(e) {
+	var options = closest(e.target, '.containerSwitcher').querySelectorAll('input');
+	var newState = options[0].checked;
+	localSwitchers.forEach(function(switcher) {
+		switchLocalView(switcher, true, newState);
+	})
 }
 
 function createSwitcher(container) {
 	var newSwitcher = globalSwitcher.cloneNode(true);
-	var inner = newSwitcher.innerHTML;
-	var suffix = '' + counter++ + '"';
+	newSwitcher.classList.remove('containerSwitcher-global');
 
-	inner = inner.replace(/"common"/g, 'common' + suffix)
-		.replace(/"it"/g, '"it' + suffix)
-		.replace(/"sphere"/g, '"sphere' + suffix);
+	localSwitchers.push(newSwitcher);
 
-	newSwitcher.innerHTML = inner;
+	newSwitcher.innerHTML = newSwitcher.innerHTML
+		.replace(/"((common)|(it)|(sphere))"/g, '$1' + localSwitchers.length);
+
 	container.appendChild(newSwitcher);
-	newSwitcher.addEventListener('change', switchView)
+
+	newSwitcher.addEventListener('change', switchView);
 }
